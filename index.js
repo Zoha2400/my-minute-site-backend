625a9f20903b496b85e67cddb0cd33df5b8409b8
Zohir <vasv29834@gmail.com>
2023-11-29 08:45:46 +0500

    Routes

--- index.js
+++ index.js
@@ -10,68 +10,65 @@ const PORT = 3000;
 
 const app = express();
 
-const fakeBd = [
-  {
-    pk: 1,
-    area: 392,
-    size: "13.6x53.6",
-    acres: "6.7",
-    style: "Неоклассика",
-    cost: "7000000",
-    data: ".......",
-    likes: 0,
-    main_photo: "http://45.153.186.140:3009/static/img/jJngaek0.png",
-    images: [
-      {
-        id: 1,
-        photo_path: "http://45.153.186.140:3009/static/img/vrFqKceg.jpg",
-      },
-      {
-        id: 2,
-        photo_path: "http://45.153.186.140:3009/static/img/vevRALGw.jpg",
-      },
-      {
-        id: 3,
-        photo_path: "http://45.153.186.140:3009/static/img/czf6Mudi.jpg",
-      },
-      {
-        id: 4,
-        photo_path: "http://45.153.186.140:3009/static/img/QezgwymL.jpg",
-      },
-      {
-        id: 5,
-        photo_path: "http://45.153.186.140:3009/static/img/lamJcjc0.jpg",
-      },
-    ],
-  },
-  {
-    pk: 2,
-    area: 123,
-    size: "18.7х20",
-    acres: "123",
-    style: "Неоклассика",
-    cost: "7000000",
-    data: "1232131",
-    likes: 0,
-    main_photo: "http://45.153.186.140:3009/static/img/5eHTyBWZ.png",
-    images: [
-      {
-        id: 6,
-        photo_path: "http://45.153.186.140:3009/static/img/UX8d1Yrj.jpg",
-      },
-      {
-        id: 7,
-        photo_path: "http://45.153.186.140:3009/static/img/RmCK2qwf.jpg",
-      },
-      {
-        id: 8,
-        photo_path: "http://45.153.186.140:3009/static/img/HM5WGXTe.jpg",
-      },
-    ],
-  },
-];
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
+
+app.get("/api/products", (req, res) => {
+  res.json(db.query("SELECT * FROM products"));
+});
+
+//registration
+app.post("/api/registrate", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    const isExist = await db.query(
+      "SELECT * FROM user_accounts WHERE email = $1",
+      [email]
+    );
+
+    if (isExist.rows.length > 0) {
+      res.status(409).send("Email already exists");
+      db.end();
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newPerson = await db.query(
+        "INSERT INTO user_accounts (email, password) values ($1, $2) RETURNING *",
+        [email, hashPassword]
+      );
+      const accountToken = await db.query(
+        "SELECT * FROM user_accounts WHERE email = $1",
+        [email]
+      );
+      console.log(accountToken.rows[0]);
+      db.end();
+      res.send(accountToken.rows[0]);
+    }
+  } else {
+    res.status(400).send("Email and password are required");
+  }
+});
+
+//Add for admin panel
+app.post("/api/add", (req, res) => {
+  res.send("add");
+});
+
+//Likes
+app.post("api/likes", async (req, res) => {
+  const pk = req.body.pk;
+  const state = req.body.state;
+
+  if (state) {
+    await db.query("SELECT * FROM products WHERE pk = $1", [pk]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
+
 /**
  * @swagger
  * /api/products:
@@ -85,7 +82,6 @@ app.use(express.json());
  *           application/json:
  *             example: fakeBd
  */
-
 /**
  * @swagger
  * /api/registrate:
@@ -114,38 +110,3 @@ app.use(express.json());
  *           application/json:
  *             example: [{"pk": 1, "area": 392, ...}, {"pk": 2, "area": 123, ...}]
  */
-
-app.get("/api/products", (req, res) => {
-  res.json(fakeBd);
-});
-
-app.post("/api/registrate", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (email && password) {
-    const isExist = await db.query(
-      "SELECT * FROM user_accounts WHERE email = $1",
-      [email]
-    );
-
-    if (isExist.rows.length > 0) {
-      res.status(409).send("Email already exists");
-      db.end();
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10);
-      const newPerson = await db.query(
-        "INSERT INTO user_accounts (email, password) values ($1, $2) RETURNING *",
-        [email, hashPassword]
-      );
-      db.end();
-      res.send(newPerson);
-    }
-  } else {
-    res.status(400).send("Email and password are required");
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
